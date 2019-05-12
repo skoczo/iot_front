599b033a3b5c698ee3d42abaf38313b37d615246
skoczo <adrian.skoczylas@gmail.com>
2019-05-12 20:59:32 +0200

    work on login page in progress

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useCookies } from 'react-cookie';
 import Dashboard from './dashboard/dashboard.js'
 import './App.css';
+import LoginPage from './auth/login.js'
 
-class App extends Component {
-  render() {
-    return (
-      <Dashboard/>
-    );
+var tokenName = 'auth-token';
+
+function App() { 
+    const [cookies, setCookie, removeCookie] = useCookies([tokenName]);
+    // setCookie(tokenName, 'val')
+    console.log(cookies[tokenName])
+    if(cookies[tokenName]) {
+      return <Dashboard/>;
+    } else {
+      return <LoginPage/>;
+    }
   }
-}
 
 export default App;
